b7c9791a31d0c0613ab52c2a3b8d23db9ee677e2
Omar Akkulov <omar-akkulov@mail.ru>
2020-12-10 15:28:43 +0300

    4

--- app/src/main/java/com/example/mybookgame/mathgame/ResultBD.java
+++ app/src/main/java/com/example/mybookgame/mathgame/ResultBD.java
@@ -1,5 +1,5 @@
 package com.example.mybookgame.mathgame;
-
+//пока в процессе
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
